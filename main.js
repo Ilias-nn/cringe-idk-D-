631fc993ed2432b27b98ad83d5b414315305b880
Ilias-nn <ilias.kyisybai@gmail.com>
2025-03-07 00:33:05 +0500

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE U').split('')
+    const titles = ('꽃과 선물을 보내고 싶었지만… 네 주소를 묻는 건 왠지 스토커 같아서 참았어 =)').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
